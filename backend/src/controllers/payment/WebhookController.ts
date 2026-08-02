5be5934574bda2786380eb7d509e86d71ddcd899
Tiago Campos <droiddzn68@gmail.com>
2026-08-02 17:38:37 -0300

    ajustando controller do webhook pra ele se comunicar com o service

--- backend/src/controllers/payment/WebhookController.ts
+++ backend/src/controllers/payment/WebhookController.ts
@@ -1,14 +1,28 @@
 import { Request, Response } from "express";
+import { WebhookService } from "../../services/payment/WebhookService.js";
+
 
 class WebhookController {
     async handle(req: Request, res: Response) {
+        try {
+            console.log("===== CHEGOU NO CONTROLLER =====");
 
-        console.log("CHEGOU!");
+            const webhookService = new WebhookService();
 
-        return res.status(200).json({
-            ok: true
-        });
+            await webhookService.execute(req.body);
 
+            return res.status(200).json({
+                ok: true,
+            });
+
+        } catch (error) {
+            console.error("ERRO NO CONTROLLER:", error);
+
+            return res.status(500).json({
+                ok: false,
+                message: "Erro ao processar webhook.",
+            });
+        }
     }
 }
 
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../lib/auth";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
   baseURL: API_URL,
